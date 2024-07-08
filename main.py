398d4460de8926e644c8721d860f0ce79a613b76
Adam Schwalm <adamschwalm@gmail.com>
2024-07-08 16:45:58 -0500

    Add basic previews

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import urllib.parse
 from websockets.sync.client import connect
 from PIL import Image
 import io
+import random
 
-#TODO: this should be separate
-client_id = str(uuid.uuid4())
+
 
 ALLOWED_RESOLUTIONS = [
     '512 x 512', '704 x 1408', '704 x 1344', '768 x 1344', '768 x 1280', '832 x 1216',
@@ -26,9 +26,10 @@ def get_available_options():
         opts["models"] = nodes["CheckpointLoaderSimple"]["input"]["required"]["ckpt_name"][0]
         opts["sampler"] = nodes["KSampler"]["input"]["required"]["sampler_name"][0]
         opts["scheduler"] = nodes["KSampler"]["input"]["required"]["scheduler"][0]
+        opts["seed_max"] = nodes["KSampler"]["input"]["required"]["seed"][1]["max"]
     return opts
 
-def queue_prompt(prompt):
+def queue_prompt(prompt, client_id):
     p = {"prompt": prompt, "client_id": client_id}
     data = json.dumps(p).encode('utf-8')
     req =  urllib.request.Request("http://{}/prompt".format(server_address), data=data)
@@ -44,8 +45,8 @@ def get_history(prompt_id):
     with urllib.request.urlopen("http://{}/history/{}".format(server_address, prompt_id)) as response:
         return json.loads(response.read())
 
-def get_images(ws, prompt, progress):
-    prompt_id = queue_prompt(prompt)['prompt_id']
+def get_images(ws, prompt, progress, client_id):
+    prompt_id = queue_prompt(prompt, client_id)['prompt_id']
     print(prompt_id)
 
     output_images = {}
@@ -62,21 +63,30 @@ def get_images(ws, prompt, progress):
                         break #Execution is done
                     else:
                         current_node = data['node']
-            elif message['type'] == 'progress' and message["data"]["node"] == "3":
+            elif message['type'] == 'progress':
+                if message["data"]["node"] == "sampler":
+                    progress_message = "Generating"
+                elif message["data"]["node"] == "save":
+                    progress_message = "Receiving"
+                else:
+                    progress_message = None
                 max = message["data"]["max"]
                 current = message["data"]["value"]
-                progress(current/max)
-
+                progress(current/max, progress_message)
         else:
-            if current_node == 'save_image_websocket_node':
+            if current_node == 'save':
                 images_output = output_images.get(current_node, [])
                 images_output.append(out[8:])
                 output_images[current_node] = images_output
-    return output_images
+            else:
+                yield [Image.open(io.BytesIO(out[8:]))]
+                print("Got binary message: ", current_node)
+    print("returning full image")
+    yield [Image.open(io.BytesIO(d)) for d in output_images["save"]]
 
 prompt_text = """
 {
-    "3": {
+    "sampler": {
         "class_type": "KSampler",
         "inputs": {
             "cfg": 8,
@@ -86,24 +96,24 @@ prompt_text = """
                 0
             ],
             "model": [
-                "4",
+                "loader",
                 0
             ],
             "negative": [
-                "7",
+                "negative_clip",
                 0
             ],
             "positive": [
-                "6",
+                "positive_clip",
                 0
             ],
             "sampler_name": "euler",
             "scheduler": "normal",
-            "seed": 8566257,
+            "seed": 0,
             "steps": 20
         }
     },
-    "4": {
+    "loader": {
         "class_type": "CheckpointLoaderSimple",
         "inputs": {
             "ckpt_name": ""
@@ -117,44 +127,44 @@ prompt_text = """
             "width": 0
         }
     },
-    "6": {
+    "positive_clip": {
         "class_type": "CLIPTextEncode",
         "inputs": {
             "clip": [
-                "4",
+                "loader",
                 1
             ],
             "text": ""
         }
     },
-    "7": {
+    "negative_clip": {
         "class_type": "CLIPTextEncode",
         "inputs": {
             "clip": [
-                "4",
+                "loader",
                 1
             ],
             "text": "bad hands"
         }
     },
-    "8": {
+    "vae": {
         "class_type": "VAEDecode",
         "inputs": {
             "samples": [
-                "3",
+                "sampler",
                 0
             ],
             "vae": [
-                "4",
+                "loader",
                 2
             ]
         }
     },
-    "save_image_websocket_node": {
+    "save": {
         "class_type": "SaveImageWebsocket",
         "inputs": {
             "images": [
-                "8",
+                "vae",
                 0
             ]
         }
@@ -162,58 +172,91 @@ prompt_text = """
 }
 """
 
-with gr.Blocks() as demo:
+def set_initial_state():
+    state = {}
+    state["client_id"] = str(uuid.uuid4())
+    state["seed"] = str()
+
     options = get_available_options()
+    state["options"] = options
+
+    model = gr.Dropdown(choices=options["models"],
+                        value=options["models"][0],
+                        interactive=True)
+
+    sampler = gr.Dropdown(value="euler_ancestral",
+                          choices=options["sampler"],
+                          interactive=True)
+
+    scheduler = gr.Dropdown(value="normal",
+                            choices=options["scheduler"],
+                            interactive=True)
+
+
+    seed = gr.Number(value=random.randrange(0, options["seed_max"]))
+    return state, model, sampler, scheduler, seed
+
+with gr.Blocks() as server:
+    state = gr.State({})
 
     gallery = gr.Gallery(label='Gallery', show_label=False, object_fit='contain', visible=True, height=768,
                          elem_classes=['resizable_area', 'main_view', 'final_gallery', 'image_gallery'],
                          elem_id='final_gallery')
     progress = gr.Progress()
+
     prompt = gr.Textbox(label="Prompt", lines=2, visible=True)
     generate_btn = gr.Button("Generate")
-    with gr.Row():
-        count = gr.Slider(1, 10, 2, step=1, label="Count")
-        resolution = gr.Dropdown(choices=ALLOWED_RESOLUTIONS,
-                                 value=DEFAULT_RESOLUTION,
-                                 label="Resolution",
-                                 interactive=True,
-                                 allow_custom_value=False,
-                                 filterable=False)
+    with gr.Accordion("Advanced", open=False):
+        negative_prompt = gr.Textbox(label="Negative Prompt", lines=2, visible=True)
+        with gr.Row():
+            count = gr.Slider(1, 10, 2, step=1, label="Count")
+            resolution = gr.Dropdown(choices=ALLOWED_RESOLUTIONS,
+                                     value=DEFAULT_RESOLUTION,
+                                     label="Resolution",
+                                     interactive=True,
+                                     allow_custom_value=False,
+                                     filterable=False)
 
-        model = gr.Dropdown(choices=options["models"],
-                            value=options["models"][0],
-                            label="Model",
-                            interactive=True,
-                            allow_custom_value=False,
-                            filterable=False)
+            model = gr.Dropdown(label="Model",
+                                allow_custom_value=False,
+                                filterable=False)
 
-    with gr.Row():
-        steps = gr.Slider(1, 80, 20, step=1, label="Steps")
-        sampler = gr.Dropdown(choices=options["sampler"], value="euler_ancestral", label="Sampler")
-        scheduler = gr.Dropdown(choices=options["scheduler"], value="normal", label="Scheduler")
+        with gr.Row():
+            steps = gr.Slider(1, 80, 20, step=1, label="Steps")
+            sampler = gr.Dropdown(label="Sampler", allow_custom_value=False,
+                                  filterable=False)
+            scheduler = gr.Dropdown(label="Scheduler", allow_custom_value=False,
+                                    filterable=False)
+        with gr.Row():
+            seed = gr.Number(label="Seed")
 
-    @generate_btn.click(inputs=[prompt, count, resolution, model, steps, sampler, scheduler], outputs=gallery)
-    def generate(text, count, resolution, model, steps, sampler, scheduler):
+    server.load(set_initial_state, outputs=[state, model, sampler, scheduler, seed])
+
+    @generate_btn.click(inputs=[prompt, count, resolution, model, steps, sampler,
+                                scheduler, negative_prompt, state, seed],
+                        outputs=gallery)
+    def generate(text, count, resolution, model, steps, sampler, scheduler, negative_prompt, state, seed):
+        client_id = state["client_id"]
         prompt = json.loads(prompt_text)
 
-        #set the seed for our KSampler node
-        prompt["3"]["inputs"]["seed"] = 5
-        prompt["3"]["inputs"]["sampler_name"] = sampler
-        prompt["3"]["inputs"]["scheduler"] = scheduler
-        prompt["3"]["inputs"]["steps"] = steps
+        prompt["sampler"]["inputs"]["seed"] = seed
+        prompt["sampler"]["inputs"]["sampler_name"] = sampler
+        prompt["sampler"]["inputs"]["scheduler"] = scheduler
+        prompt["sampler"]["inputs"]["steps"] = steps
 
         width, height = resolution.split(" x ")
 
-        prompt["4"]["inputs"]["ckpt_name"] = model
+        prompt["loader"]["inputs"]["ckpt_name"] = model
         prompt["latent_image"]["inputs"]["batch_size"] = count
         prompt["latent_image"]["inputs"]["width"] = width
         prompt["latent_image"]["inputs"]["height"] = height
-        prompt["6"]["inputs"]["text"] = text
+        prompt["positive_clip"]["inputs"]["text"] = text
+        prompt["negative_clip"]["inputs"]["text"] = negative_prompt
 
         ws = connect("ws://{}/ws?clientId={}".format(server_address, client_id), max_size=3000000)
-        images = get_images(ws, prompt, progress)
-        for node_id in images:
-            return [Image.open(io.BytesIO(d)) for d in images[node_id]]
+        images = get_images(ws, prompt, progress, client_id)
+        for img_list in images:
+            print("got a list of images: ", len(img_list))
+            yield img_list
 
-
-demo.launch()
+server.launch()
